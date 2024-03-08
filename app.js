f17a6745768e2a94993a70302273e44fb1ee3d4d
Qitmeershehzad <qitmeershehzad@gmail.com>
2024-03-08 22:59:43 +0500

    Java script 1 class

---
+++ app.js
@@ -0,0 +1,14 @@
+/*
+string "" 
+*/
+// alert("Welcome Qitmeer");
+// alert("Welcome Saad")
+// alert("Welacome Shehzad")
+// alert("Welcome Safan")
+alert("Welcome ")
+console.log(3+2);
+// cansole.log(8-5);
+console.log(9-7);
+console.log(7*9);
+console.log(10/5);
+console.log(100*100);
